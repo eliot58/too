8fd8b3b54f1c131d48789a88c3f0c7d65e08c8ba
Aimedin Chorobaev <eliothab58@gmail.com>
2023-04-15 22:37:27 +0600

    fix search

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import *
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('information/', InformationView.as_view()),
     path('resolve/', ResolveView.as_view()),
     path('gallery/', GalleryView.as_view()),
-    re_path(r'^search/(?P<cat>(ads|news|info|resolve|gallery))/<str:q>/$', SearchView.as_view()),
+    path('search/<str:cat>/<str:q>/', SearchView.as_view()),
     path('address/', AddressView.as_view()),
     # path('agriculture/', AgricultureView.as_view()),
     path('culture/', CultureView.as_view()),
